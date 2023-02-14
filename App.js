71fd705b4321cfc3d43b8fa30667d37a790a4b3a
Elena Solomko <lena06051981be@gmail.com>
2023-02-14 16:48:54 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+} from "react-native";
+// import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const inputHandler = (text) => setValue(text);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Hi everywere! Does it works? Yeap. Ok</Text>
-      <StatusBar style="auto" />
+        <TextInput
+          placeholder="Type text"
+          value={value}
+          onChangeText={inputHandler}
+        />    
+        {/* <View style={styles.container}>
+        <Text>Open up App.js to start working on your app! Hi everywere! Does it works? Yeap. Ok</Text>
+        <StatusBar style="auto" />
+        </View> */}
     </View>
   );
 }
@@ -13,8 +28,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 30
   },
 });
+
+
+
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app! Hi everywere! Does it works? Yeap. Ok</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
